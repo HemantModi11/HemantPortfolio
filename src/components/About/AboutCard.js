beab8db1b85ecea3b9206cb0296e2938a0dbdd17
Hemant Modi <90129538+HemantModi11@users.noreply.github.com>
2023-11-10 02:17:44 +0530

    Update AboutCard.js

--- src/components/About/AboutCard.js
+++ src/components/About/AboutCard.js
@@ -8,22 +8,22 @@ function AboutCard() {
       <Card.Body>
         <blockquote className="blockquote mb-0">
           <p style={{ textAlign: "justify" }}>
-            Hi Everyone, I am <span className="purple">Swetabh Shreyam </span>
-            from <span className="purple"> New Delhi, India.</span>
+            Hi Everyone, I am <span className="purple">Hemant Modi </span>
+            from <span className="purple"> Jaipur, India.</span>
             <br /> I am a Third Year student pursuing B.Tech.
-            in Information Technology from Vellore University of Technology.
+            in Computer Science and Engineering from Vellore University of Technology.
             <br />
-            Additionally, I am enthusiastic about Web Devlopment.
+            Additionally, I am enthusiastic about Machine Learning.
             <br />
             <br />
             Some of my hobbies are 
           </p>
           <ul>
             <li className="about-activity">
-              <ImPointRight /> Football
+              <ImPointRight /> Cricket
             </li>
             <li className="about-activity">
-              <ImPointRight /> Astronomy
+              <ImPointRight /> Gym
             </li>
             <li className="about-activity">
               <ImPointRight /> Programming
